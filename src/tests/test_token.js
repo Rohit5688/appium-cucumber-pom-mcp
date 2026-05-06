ba86ddc4bdce32e020bdd87e37f38eaf95a38802
rohit5688 <rohit.sakhawalkar@gmail.com>
2026-05-07 01:26:35 +0530

    chore: remove scratch scripts and temporary artifacts

--- src/tests/test_token.js
+++
@@ -1,5 +0,0 @@
-import { TokenBudgetService } from '../../dist/services/TokenBudgetService.js';
-const service = TokenBudgetService.getInstance();
-const tokens = service.estimateTokens('Hello, world!');
-console.assert(tokens >= 2 && tokens <= 5, 'Token estimate off');
-console.log('Token estimate test passed');
